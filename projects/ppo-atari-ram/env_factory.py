8dd791469e5cb7b5280669f61eab450e07c27cb5
1998x-stack <2484230700@qq.com>
2026-06-13 19:27:51 +0800

    feat: add Atari RAM env factory with vectorized envs

---
+++ projects/ppo-atari-ram/env_factory.py
@@ -0,0 +1,37 @@
+"""Atari RAM environment factory with vectorized envs."""
+import numpy as np
+import gymnasium as gym
+
+
+def make_ram_env(env_id: str, num_envs: int = 4, seed: int = 42):
+    """Create AsyncVectorEnv of Atari RAM environments.
+
+    Observation: (128,) uint8 scaled to [0, 1].
+    Action space: discrete, env-specific size.
+    """
+
+    def _make_env(rank: int):
+        def _init():
+            env = gym.make(env_id, max_episode_steps=108000)
+            env = gym.wrappers.RecordEpisodeStatistics(env)
+            env = gym.wrappers.TransformObservation(
+                env, lambda obs: obs.astype(np.float32) / 255.0
+            )
+            env.reset(seed=seed + rank)
+            return env
+        return _init
+
+    envs = gym.vector.AsyncVectorEnv([_make_env(i) for i in range(num_envs)])
+    return envs
+
+
+def get_env_info(env_id: str):
+    """Return (obs_dim, n_actions) for a given env ID.
+
+    Returns (128, n_actions) for RAM envs. Creates and disposes a temp env.
+    """
+    env = gym.make(env_id)
+    obs_dim = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+    env.close()
+    return obs_dim, n_actions
